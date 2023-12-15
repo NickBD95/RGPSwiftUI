5cd255f70deb4a960a43c131a3e41b2b854b362c
Nickolai Grachev <nickbd95@gmail.com>
2023-12-15 12:50:40 +0300

    Initial Commit

---
+++ RGPSwiftUI/RGPSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGPSwiftUIApp.swift
+//  RGPSwiftUI
+//
+//  Created by Nikolai Grachev on 15.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct RGPSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
